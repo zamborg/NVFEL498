f905057ed13c65c6ba7eeed1914115dbb131e751
Gabe Mudel <gmudel@gl-login2.arc-ts.umich.edu>
2021-03-09 00:52:29 -0500

    committing to pull

--- Processing/help.py
+++ Processing/help.py
@@ -1,4 +1,7 @@
 import pandas as pd
+from utils import *
 
-df = pd.read_csv("ICE_trip7636.csv")
-print(len(df['Latitude[deg]']))
+
+trip = pd.read_csv("ICE_trip7636.csv")
+f = distance_functor()
+trip['Distance[km]'] = trip.apply(lambda df : f(df['Latitude[deg]'], df['Longitude[deg]']), axis=1)
