e0588940d60d8dcf2fecf2a76a61b4670af20077
Gabe Mudel <gmudel@gl-login2.arc-ts.umich.edu>
2021-03-10 03:19:30 -0500

    ran first regression. Very low r2.

--- Processing/create_trip_df.py
+++ Processing/create_trip_df.py
@@ -2,46 +2,50 @@ import glob
 import os
 from utils import *
 import pandas as pd
+import numpy as np
 
 processed_data_path = '/nfs/turbo/midas-applied-ds/Data/Processed'
-paths = glob.glob(os.path.join(processed_data_path, 'ICE_trips', '*.csv'))
+paths = glob.glob(os.path.join(processed_data_path, 'ICE_trips', 'ICE_trip*.csv'))
 alltrips_path = os.path.join(processed_data_path, 'ICE_trips', 'alltrips.csv')
 
-df = pd.read_csv(alltrips_path)
+num_trips = 17737
+l = []
 
-for i in range(17739):
+d = {
+  'TripId' : [],
+  'Aggressivity' : [],
+  'Aggressiveness' : [],
+  'Fuel Consumed[L]' : [],
+  'Distance[km]' : [],
+}
+
+for i, path in enumerate(paths):
   if i % 1000 == 0:
     print(i)
-  path = os.path.join(processed_data_path, 'ICE_trips', f'ICE_trip{i}.csv')
-  trip = pd.read_csv(path)
+  #path = os.path.join(processed_data_path, 'ICE_trips', f'ICE_trip{i}.csv')
+  try:
+    trip = pd.read_csv(path)
+    fuel_comp = trip['Fuel Consumption[km/L]']
+    distance = trip['Distance[km]']
+    
+    pf = aggressivity(trip)
+    pke = aggressiveness(trip)
+    total_d = np.sum(distance)
+    total_fuel = np.sum(distance / fuel_comp)
+    
+    d['TripId'].append(i)
+    d['Aggressivity'].append(pf)
+    d['Aggressiveness'].append(pke)
+    d['Fuel Consumed[L]'].append(total_fuel)
+    d['Distance[km]'].append(total_d)
+  except Exception as e:
+    print(f'Error opening file {i}')
+    print(e)
+    l.append(np.nan)
   '''
-  pf = aggressivity(trip)
-  pke = aggressiveness(trip)
-  d['TripId'].append(i)
-  d['Aggressivity'].append(pf)
-  d['Aggressiveness'].append(pke)
   '''
-  fuel_comp = trip['Fuel Consumption[km/L]']
-  distance = trip['Distance[km]']
-  temp_f = []
-  final_f = []
-  final_d = []
-  for i in range(len(distance)):
-    d = distance[i]
-    f = fuel_comp[i]
-    if d == 0 or pd.isna(d) and not pd.isna(f):
-      temp_f.append(f)
-    else:
-      final_f.append(np.array(temp_f).mean())
-      temp_f = []
-      final_d.append(d)
-  total_d = np.sum(np.array(final_d))
-  total_fuel = 1/np.sum(np.array(final_f) / np.array(final_d))
-  print(total_fuel, total_d, 1.6* total_d / .26* total_fuel)
 
 df = pd.DataFrame(d)
-f = open(alltrips_path)
+f = open(alltrips_path, 'w')
 df.to_csv(f)
-  
-  
   
---
+++ Processing/simple_regression.py
@@ -0,0 +1,65 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+df = pd.read_csv('../../../Data/Processed/ICE_trips/alltrips.csv')
+print(df)
+num_trips = len(df)
+split1 = num_trips * 7 // 10
+split2 = split1 + num_trips * 3 // 20
+
+pke = df['Aggressiveness']
+pf = df['Aggressivity']
+fuel = df['Fuel Consumed[L]']
+
+idx = pke.isna() | pf.isna() | fuel.isna() | np.isinf(pke) | np.isinf(pf) | np.isinf(fuel)
+pke = pke[~idx]
+pf = pf[~idx]
+fuel = fuel[~idx]
+
+
+
+print(pke[pke.isna()])
+print(pf[pf.isna()])
+print(fuel[fuel.isna()])
+
+print(sum(pke))
+print(sum(pf))
+print(sum(fuel))
+
+
+X1_train = np.array(pke[:split1]).reshape(-1, 1)
+X1_val = np.array(pke[split1:split2]).reshape(-1, 1)
+X1_test = np.array(pke[split2:]).reshape(-1, 1)
+
+X2_train = np.array(pf[:split1]).reshape(-1, 1)
+X2_val = np.array(pf[split1:split2]).reshape(-1, 1)
+X2_test = np.array(pf[split2:]).reshape(-1, 1)
+
+y_train = np.array(fuel[:split1]).reshape(-1, 1)
+y_val = np.array(fuel[split1:split2]).reshape(-1, 1)
+y_test = np.array(fuel[split2:]).reshape(-1, 1)
+
+reg1 = LinearRegression().fit(X1_train, y_train)
+reg2 = LinearRegression().fit(X2_train, y_train)
+
+train1_score = reg1.score(X1_train, y_train)
+train2_score = reg2.score(X2_train, y_train)
+val1_score = reg1.score(X1_val, y_val)
+val2_score = reg2.score(X2_val, y_val)
+
+y1_pred = reg1.predict(X1_test)
+
+print('Train Scores')
+print(f'PKE: {train1_score}\tPF: {train2_score}')
+print('Val Scores')
+print(f'PKE: {val1_score}\tPF: {val2_score}')
+
+plt.scatter(X1_test, y_test,color='black')
+plt.plot(X1_test, y1_pred, color='blue', linewidth=3)
+
+plt.xticks(())
+plt.yticks(())
+
+plt.savefig('pke_simplereg.png')
