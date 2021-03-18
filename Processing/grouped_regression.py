fb0815deb57bf831402a7a247b7afec14a093454
Gabe Mudel <gmudel@gl-login2.arc-ts.umich.edu>
2021-03-18 15:06:01 -0400

    ran reg

--- Processing/grouped_regression.py
+++ Processing/grouped_regression.py
@@ -3,35 +3,32 @@ import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
 
-df = pd.read_csv('../../../Data/Processed/ICE_trips/alltrips.csv')
-print(df)
-num_trips = len(df)
-split1 = num_trips * 7 // 10
-split2 = split1 + num_trips * 3 // 20
-
-pke = df['Aggressiveness']
-w = df['Weight']
-d = df['Displacement']
-fuel = df['Fuel Economy[mpg]']
+df = pd.read_csv('../../../Data/Processed/ICE_trips/alltrips_with_weight_and_disp.csv')
 
 X = df[['Aggressiveness', 'Weight', 'Displacement']]
-Y = fuel
+Y = df['Fuel Economy[mpg]']
+
+pke = X['Aggressiveness']
+w = X['Weight']
+d = X['Displacement']
+fuel = Y
 
 idx = pke.isna() | fuel.isna() | w.isna() | d.isna() | np.isinf(pke) | np.isinf(fuel) | np.isinf(w) | np.isinf(d)
 print('any true', idx.any())
 
 X = X[~idx]
+Y = Y[~idx]
 
-print(pke[pke.isna()])
-print(fuel[fuel.isna()])
+num_trips = len(X)
+split1 = num_trips * 7 // 10
+split2 = split1 + num_trips * 3 // 20
 
-print(sum(pke))
-print(sum(fuel))
+X_train = np.array(X[:split1]).reshape(-1, 3)
+X_val = np.array(X[split1:split2]).reshape(-1, 3)
+X_test = np.array(X[split2:]).reshape(-1, 3)
 
-X_train = np.array(X[:split1]).reshape(-1, 1)
-X_val = np.array(X[split1:split2]).reshape(-1, 1)
-X_test = np.array(X[split2:]).reshape(-1, 1)
-
+print(X_train.shape)
+print(X_val.shape)
 y_train = np.array(Y[:split1]).reshape(-1, 1)
 y_val = np.array(Y[split1:split2]).reshape(-1, 1)
 y_test = np.array(Y[split2:]).reshape(-1, 1)
@@ -48,8 +45,8 @@ print(f'PKE: {train_score}')
 print('Val Scores')
 print(f'PKE: {val_score}')
 
-plt.scatter(X_val, y_val, color='black')
-plt.plot(X_val, y_pred, color='blue', linewidth=3)
+plt.scatter(X_val[:, 0], y_val, color='black')
+plt.plot(X_val[:, 0], y_pred, color='blue', linewidth=3)
 
 plt.xticks((np.arange(0, 500000, 100000)))
 plt.yticks((np.arange(0, 65, 5)))
