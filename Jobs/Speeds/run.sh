f607d84fe9a008ad82639eac7a02cd835e6542a9
Zubin T Aysola <aysola@gl-login1.arc-ts.umich.edu>
2021-03-27 23:57:52 -0400

    velocity code working

---
+++ Jobs/Speeds/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# The interpreter used to execute the script
+
+#“#SBATCH” directives that convey submission options:
+
+#SBATCH --job-name=Velocity
+#SBATCH --mail-user=aysola@umich.edu
+#SBATCH --mail-type=END
+#SBATCH --cpus-per-task=10
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem-per-cpu=8000m 
+#SBATCH --time=1:00:00
+#SBATCH --account=eecs498s009w21_class
+#SBATCH --partition=standard
+
+python velocity.py
