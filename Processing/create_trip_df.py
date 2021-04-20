e4c684a7765bb12e4c59e5be730a18aabfcafdae
Gabe Mudel <gmudel@gl-login2.arc-ts.umich.edu>
2021-04-20 16:43:27 -0400

    help

--- Processing/create_trip_df.py
+++ Processing/create_trip_df.py
@@ -1,11 +1,11 @@
 import glob
 import os
-from utils import aggressivity, aggressiveness
+from utils import aggressivity, aggressiveness, get_fuel, get_distances
 import pandas as pd
 import numpy as np
 
 paths = os.listdir('/nfs/turbo/midas-applied-ds/Data/Processed/HEV_trips/')
-alltrips_path = ('/nfs/turbo/midas-applied-ds/Project/NVFEL498/all_HEV_trips.csv/')
+alltrips_path = ('/nfs/turbo/midas-applied-ds/Project/NVFEL498/all_HEV_trips.csv')
 
 l = []
 
@@ -20,23 +20,24 @@ d = {
   'Fuel Rate[gpm]' : [],
 }
 
+nonEVs = pd.read_csv('/nfs/turbo/midas-applied-ds/Project/NVFEL498/VED_Static_Data_ICE_HEV.csv')
+
 def nacheck(x):
   return pd.isna(x) or np.isinf(x) or x == 0
 
 for i, path in enumerate(paths):
   if i % 1000 == 0:
     print(i)
-  #path = os.path.join(processed_data_path, 'ICE_trips', f'ICE_trip{i}.csv')
+  path = os.path.join('/nfs/turbo/midas-applied-ds/Data/Processed/HEV_trips/', path)
   try:
     trip = pd.read_csv(path)
-    fuel_comp = trip['Fuel Consumption[km/L]']
-    distance = trip['Distance[km]']
+    distance = get_distances(trip)
     
     pf = aggressivity(trip)
     pke = aggressiveness(trip)
     print(pke)
     total_d = np.sum(distance)
-    total_fuel = np.sum(distance / fuel_comp)
+    total_fuel = get_fuel(trip, nonEVs)
     
     raw_trip_ids = trip['Trip'].unique()
     assert(len(raw_trip_ids) == 1)
@@ -54,7 +55,7 @@ for i, path in enumerate(paths):
     d['Aggressiveness'].append(pke)
     d['Distance[km]'].append(total_d)
     d['Fuel Consumed[L]'].append(total_fuel)
-    d['Fuel Rate[gpm]'].append(total_fuel / total_d)
+    d['Fuel Rate[gpm]'].append(total_fuel / total_d / 2.352)
   except Exception as e:
     print(f'Error opening file {i}')
     print(e)
